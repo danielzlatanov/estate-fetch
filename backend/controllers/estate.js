4b203e200c3702d6d299395af66be571699510a4
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-09-04 16:55:52 +0300

    update estate controller to save scraped data in DB

--- backend/controllers/estate.js
+++ backend/controllers/estate.js
@@ -1,14 +1,38 @@
+const scraper = require('../services/scraper');
+const Estate = require('../models/Estate');
+
+const scrapeAndSaveEstateData = async (req, res) => {
+	try {
+		const realEstateData = await scraper.scrapeRealEstateData();
+
+		if (realEstateData == null) {
+			throw new Error('Scraped data is not an array');
+		}
+
+		const savedEstates = await Promise.all(
+			realEstateData.map(async estateData => {
+				const estate = new Estate(estateData);
+				return await estate.save();
+			})
+		);
+
+		res.json({ message: 'Real estate data scraped and saved successfully.', savedEstates });
+	} catch (err) {
+		console.error('Error scraping and saving real estate data:', err);
+		res.status(500).json({ error: 'Internal server error' });
+	}
+};
+
 const getAllEstates = (req, res) => {
-	console.log('receiving all');
 	res.json({ message: 'You have received all of the estates.' });
 };
 
 const getEstateById = (req, res) => {
-	console.log('receiving one');
 	res.json({ message: 'You have received specific info regarding an estate.' });
 };
 
 module.exports = {
 	getAllEstates,
 	getEstateById,
+	scrapeAndSaveEstateData,
 };
