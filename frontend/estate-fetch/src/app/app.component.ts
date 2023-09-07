eb44f3c042a9c609e2ac99baefbfa8bdae7d9dd3
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-09-07 13:22:13 +0300

    added header cmp and mobile menu with nav links

--- frontend/estate-fetch/src/app/app.component.ts
+++ frontend/estate-fetch/src/app/app.component.ts
@@ -1,60 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from './services/api.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  response: any;
-  first: any;
-  second: any;
-
-  constructor(private apiService: ApiService) {}
-
-  ngOnInit(): void {
-    this.testFirst();
-    this.testSecond();
-  }
-
-  startScraping() {
-    this.testScrape();
-  }
-
-  testScrape() {
-    this.apiService.testScrape().subscribe(
-      (data: any) => {
-        this.response = data;
-      },
-      (err: any) => {
-        console.error('Error:', err);
-        this.response = 'Error while scraping.';
-      }
-    );
-  }
-
-  testFirst() {
-    this.apiService.testFirstRoute().subscribe(
-      (data: any) => {
-        this.first = data.message;
-      },
-      (err: any) => {
-        console.error('Error:', err);
-        this.first = 'Error receiving first route.';
-      }
-    );
-  }
-
-  testSecond() {
-    this.apiService.testSecondRoute().subscribe(
-      (data: any) => {
-        this.second = data.message;
-      },
-      (err: any) => {
-        console.error('Error:', err);
-        this.second = 'Error receiving second route.';
-      }
-    );
-  }
-}
+export class AppComponent {}
