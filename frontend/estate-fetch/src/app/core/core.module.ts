0413d872589ed2a57ac7af983a65e751e7e852d9
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-09-11 12:40:28 +0300

    implement responsive footer design with styles
    needs further small-screen optimization

--- frontend/estate-fetch/src/app/core/core.module.ts
+++ frontend/estate-fetch/src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [HomeComponent, HeaderComponent],
+  declarations: [HomeComponent, HeaderComponent, FooterComponent],
   imports: [CommonModule],
-  exports: [HomeComponent, HeaderComponent],
+  exports: [HomeComponent, HeaderComponent, FooterComponent],
 })
 export class CoreModule {}
