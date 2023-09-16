c877903a344dfe6c4091a01c310bdb82d8947bd6
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-09-16 17:48:25 +0300

    added initial details cmp with basic structure

--- frontend/estate-fetch/src/app/estate/estate-routing.module.ts
+++ frontend/estate-fetch/src/app/estate/estate-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CatalogComponent } from './catalog/catalog.component';
+import { DetailsComponent } from './details/details.component';
 
 const routes: Routes = [
   {
     path: 'catalog',
     component: CatalogComponent,
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent,
   },
 ];
 
--- frontend/estate-fetch/src/app/estate/estate.module.ts
+++ frontend/estate-fetch/src/app/estate/estate.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { EstateRoutingModule } from './estate-routing.module';
 import { CatalogComponent } from './catalog/catalog.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
-  declarations: [CatalogComponent],
+  declarations: [CatalogComponent, DetailsComponent],
   imports: [CommonModule, EstateRoutingModule],
 })
 export class EstateModule {}
