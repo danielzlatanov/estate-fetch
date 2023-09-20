0efd8ace190cf423fe0149d64febaa8764585113
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-09-20 19:29:37 +0300

    added details cmp logic to fetch and log estate data

--- frontend/estate-fetch/src/app/estate/details/details.component.ts
+++ frontend/estate-fetch/src/app/estate/details/details.component.ts
@@ -1,6 +1,8 @@
 import { scrollToTop } from 'src/app/shared/helpers/scrollToTop';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IEstate } from 'src/app/shared/interfaces/estate';
+import { EstateService } from '../estate.service';
 
 @Component({
   selector: 'app-details',
@@ -8,9 +10,16 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./details.component.css'],
 })
 export class DetailsComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  estate: IEstate | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private estateService: EstateService
+  ) {}
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
     this.route.fragment.subscribe((fragment) => {
       if (fragment === 'top') {
         setTimeout(() => {
@@ -18,5 +27,17 @@ export class DetailsComponent implements OnInit {
         }, 100);
       }
     });
+
+    if (id) {
+      this.estateService.getEstateById(id).subscribe({
+        next: (data: IEstate) => {
+          this.estate = data;
+          console.log('data received', data);
+        },
+        error: (error: any) => {
+          console.error('An error occurred:', error);
+        },
+      });
+    }
   }
 }
