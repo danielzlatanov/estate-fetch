eb055fb49f9319d4e99434e37de32b0afcd550fe
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-09-20 16:29:47 +0300

    added catalog cmp logic to fetch and log all estate data

--- frontend/estate-fetch/src/app/estate/catalog/catalog.component.ts
+++ frontend/estate-fetch/src/app/estate/catalog/catalog.component.ts
@@ -1,14 +1,17 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { EstateService } from '../estate.service';
+import { IEstate } from 'src/app/shared/interfaces/estate';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.css'],
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit {
+  estates: IEstate[] = [];
   isSmallScreen: boolean = false;
 
-  constructor() {
+  constructor(private estateService: EstateService) {
     this.checkScreenSize();
   }
 
@@ -17,7 +20,23 @@ export class CatalogComponent {
     this.checkScreenSize();
   }
 
+  ngOnInit(): void {
+    this.fetchEstates();
+  }
+
   private checkScreenSize(): void {
     this.isSmallScreen = window.innerWidth <= 768;
+  }
+
+  private fetchEstates(): void {
+    this.estateService.getData().subscribe({
+      next: (data: IEstate[]) => {
+        this.estates = data;
+        console.log('fetched all estates:', this.estates);
+      },
+      error: (error: any) => {
+        console.error('An error occurred:', error);
+      },
+    });
   }
 }
