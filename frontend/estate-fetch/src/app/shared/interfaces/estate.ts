69f28fc80bad5904b263e1a18f6f1846a3d34d8b
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-09-20 16:11:52 +0300

    added estate interface for frontend data modeling

---
+++ frontend/estate-fetch/src/app/shared/interfaces/estate.ts
@@ -0,0 +1,17 @@
+export interface IEstate {
+  _id: string;
+  title: string;
+  location: string;
+  price: string;
+  sqm: string;
+  image: string;
+  phone: string;
+  area: string;
+  floor: string;
+  construction: string;
+  description: string;
+  url: string;
+  realtor: string;
+  realtorLogo: string;
+  realtorAddress: string;
+}
