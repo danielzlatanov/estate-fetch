7c55299c63c0812e7c18fffc95e8404595ba2ca7
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-09-22 13:25:38 +0300

    implemented responsive img carousel in details cmp
    the keen-slider touch carousel enhances UX with smooth image navigation (static images)

--- frontend/estate-fetch/src/app/estate/estate.module.ts
+++ frontend/estate-fetch/src/app/estate/estate.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { EstateRoutingModule } from './estate-routing.module';
 import { CatalogComponent } from './catalog/catalog.component';
 import { DetailsComponent } from './details/details.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [CatalogComponent, DetailsComponent],
-  imports: [CommonModule, EstateRoutingModule],
+  imports: [CommonModule, EstateRoutingModule, SharedModule],
 })
 export class EstateModule {}
--- frontend/estate-fetch/src/app/shared/slider/slider.component.ts
+++ frontend/estate-fetch/src/app/shared/slider/slider.component.ts
@@ -7,4 +7,29 @@ import KeenSlider, { KeenSliderInstance } from 'keen-slider';
   styleUrls: ['./slider.component.css'],
 })
 export class SliderComponent {
+  @ViewChild('sliderRef') sliderRef!: ElementRef<HTMLElement>;
+
+  currentSlide: number = 1;
+  dotHelper: Array<Number> = [];
+  slider: KeenSliderInstance = null!;
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.slider = new KeenSlider(this.sliderRef.nativeElement, {
+        initial: this.currentSlide,
+        slideChanged: (s) => {
+          this.currentSlide = s.track.details.rel;
+        },
+      });
+      this.dotHelper = [
+        ...Array(this.slider.track.details.slides.length).keys(),
+      ];
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.slider) {
+      this.slider.destroy();
+    }
+  }
 }
