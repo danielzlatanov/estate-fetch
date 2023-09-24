e0fe8295e72fcca8d5d167666951ac8409dcfcc7
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-09-24 14:10:50 +0300

    adapt 'index.js' for serverless and express.js hosting

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,9 @@ const expressConfig = require('./config/express');
 const dbConfig = require('./config/database');
 const estateRoutes = require('./routes/estateRoutes');
 
-start();
-async function start() {
-	const app = express();
+const app = express();
 
+(async () => {
 	await dbConfig(app);
 	expressConfig(app);
 
@@ -19,7 +18,17 @@ async function start() {
 	app.use('/api', estateRoutes);
 
 	const port = process.env.PORT || 8000;
-	app.listen(port, () => {
-		console.log(`Server is running on port ${port}...`);
-	});
-}
+
+	if (!process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+		app.listen(port, () => {
+			console.log(`Server is running on port ${port}...`);
+		});
+	}
+})();
+
+exports.handler = async (event, context) => {
+	return {
+		statusCode: 200,
+		body: JSON.stringify({ message: 'Serverless function executed successfully' }),
+	};
+};
