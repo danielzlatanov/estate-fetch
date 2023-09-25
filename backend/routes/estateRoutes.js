99ad781cf7df95d09c33661d885fc42b0e1e4e06
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-09-25 20:46:01 +0300

    protect scrape route with secret token verification

--- backend/routes/estateRoutes.js
+++ backend/routes/estateRoutes.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
 const estateController = require('../controllers/estate');
+const secretToken = process.env.SECRET_TOKEN;
 
-router.get('/scrape', estateController.scrapeAndSaveEstateData);
+function verifyToken(req, res, next) {
+	const token = req.headers['x-secret-token'];
+	if (!token || token !== secretToken) {
+		return res.status(401).json({ message: 'Unauthorized' });
+	}
+	next();
+}
+
+router.get('/scrape', verifyToken, estateController.scrapeAndSaveEstateData);
 router.get(['/estates', '/estates/search'], estateController.getAllEstates);
 router.get('/estates/:id', estateController.getEstateById);
 
