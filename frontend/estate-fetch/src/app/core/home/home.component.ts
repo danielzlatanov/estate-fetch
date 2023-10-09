d7c2a90df31f174dfb7a9bd9458d2f47d26340ac
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-10-09 14:13:07 +0300

    refactor FAQ section to use ngFor loops

--- frontend/estate-fetch/src/app/core/home/home.component.ts
+++ frontend/estate-fetch/src/app/core/home/home.component.ts
@@ -5,4 +5,57 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  showAnswer: boolean[] = Array(8).fill(false);
+  faqItems = [
+    {
+      question: 'What is Estate Fetch?',
+      answer:
+        'A powerful web scraping tool designed to efficiently gather real estate data from online sources and provide you with the saved data.',
+    },
+
+    {
+      question: 'How does it work?',
+      answer:
+        'It uses advanced web scraping techniques to extract property information from websites, giving you up-to-date data.',
+    },
+    {
+      question: 'What kind of real estate data can I access?',
+      answer:
+        'Estate Fetch allows you to access a wide range of real estate data, including property details, images, and more.',
+    },
+    {
+      question: 'What types of websites can Estate Fetch scrape data from?',
+      answer:
+        'Currently, it is capable of scraping data from imot.bg, one of the largest real estate websites in Bulgaria. We are actively working on expanding our sources.',
+    },
+    {
+      question: 'Does it handle errors and interruptions?',
+      answer:
+        'Absolutely. Estate Fetch includes a reliable error handling system and a retry mechanism to ensure uninterrupted data retrieval.',
+    },
+    {
+      question:
+        'How often is it updated to stay compatible with website changes?',
+      answer:
+        'It is regularly updated to adapt to source changes, ensuring consistent data extraction.',
+    },
+    {
+      question: 'Is it compatible with my browser and operating system?',
+      answer:
+        'It is designed to work seamlessly across different browsers and operating systems for your ease.',
+    },
+    {
+      question: 'Is it suitable for professionals and beginners?',
+      answer:
+        'Yes, it is easy to use for both experienced users and beginners, thanks to its user-friendly design.',
+    },
+  ];
+
+  faqItemsLeft = this.faqItems.slice(0, this.faqItems.length / 2);
+  faqItemsRight = this.faqItems.slice(this.faqItems.length / 2);
+
+  toggleAnswer(index: number) {
+    this.showAnswer[index] = !this.showAnswer[index];
+  }
+}
