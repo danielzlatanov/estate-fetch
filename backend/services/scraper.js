5b2e8d78e10a6701f6766f5181d151cb846913b2
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-10-10 13:02:38 +0300

    implemented data scraping logic for all pages

--- backend/services/scraper.js
+++ backend/services/scraper.js
@@ -15,8 +15,6 @@ async function scrapeRealEstateData() {
 		await page.click('input[type="button"][value="Т Ъ Р С И"]');
 		await delay(5000);
 
-		const listingUrls = await page.$$eval('a.lnk3', links => links.map(link => link.href));
-		const validListingUrls = listingUrls.filter(url => !url.startsWith('javascript:'));
 		await page.waitForSelector('.pageNumbersInfo');
 		const pagesInfo = (await page.$eval('.pageNumbersInfo', el => el.textContent)).trim();
 		let totalPages;
@@ -28,7 +26,6 @@ async function scrapeRealEstateData() {
 			console.error("Total pages weren't retrieved");
 		}
 
-		const realEstateData = [];
 		const pageLinks = [];
 		for (let i = 1; i <= totalPages; i++) {
 			const pageLink = `https://www.imot.bg/pcgi/imot.cgi?act=3&slink=9u9dut&f1=${i}`;
@@ -36,110 +33,122 @@ async function scrapeRealEstateData() {
 		}
 		let pageCount = 0;
 
-		for (const url of validListingUrls) {
-			try {
-				await page.goto(url);
 		console.log('page links constructed:', pageLinks);
 
-				const isPriceUnavailable = await page.evaluate(() => {
-					const textToCheck = 'При запитване';
-					return document.body.textContent.includes(textToCheck);
-				});
+		for (const pageLink of pageLinks) {
+			pageCount++;
+			console.log(`scraping page ${pageCount} of ${totalPages} with link: ${pageLink}...`);
+			await page.goto(pageLink);
+			await delay(3000);
+			await page.screenshot({ path: `page${pageCount}.png`, fullPage: true });
 
-				if (isPriceUnavailable) {
-					console.log(`Price is unavailable for URL: ${url}`);
-					continue;
-				}
+			const listingUrls = await page.$$eval('a.lnk3', links => links.map(link => link.href));
+			const validListingUrls = listingUrls.filter(url => !url.startsWith('javascript:'));
 
-				const images = [];
+			const realEstateData = [];
 
-				const hasMultipleImages = await page.evaluate(() => {
-					const divIm = document.querySelector('div.im');
-					return divIm !== null;
-				});
+			for (const url of validListingUrls) {
+				try {
+					await page.goto(url);
 
-				if (hasMultipleImages) {
-					const imageSelectors = await page.$$eval('div.im a', els => els.map(el => el.dataset.link));
-					images.push(...imageSelectors.map(imgUrl => imgUrl.trim()));
-				} else {
-					const imgUrl = (await page.$eval('#bigPictureCarousel', el => el.src)).trim();
-					images.push(imgUrl);
-				}
+					const isPriceUnavailable = await page.evaluate(() => {
+						const textToCheck = 'При запитване';
+						return document.body.textContent.includes(textToCheck);
+					});
 
-				await page.waitForSelector('.title');
-				await page.waitForSelector('.location');
-				await page.waitForSelector('#cena');
-				await page.waitForSelector('#cenakv');
-				await page.waitForSelector('#bigPictureCarousel');
-				await page.waitForSelector('.phone');
-				await page.waitForSelector('.adParams div:first-child');
-				await page.waitForSelector('.adParams div:nth-child(2)');
-				await page.waitForSelector('.adParams div:nth-child(3)');
-				await page.waitForSelector('#description_div');
-				await page.waitForSelector('.AG');
-				await page.waitForSelector('.AG .name');
-				await page.waitForSelector('.AG .logo img');
-				await page.waitForSelector('.AG .adress');
-
-				const title = (await page.$eval('.title', el => el.textContent)).trim();
-				const titleNoPrice = extractTitleWithoutPrice(title);
-
-				const location = (await page.$eval('.location', el => el.textContent)).trim();
-				const price = (await page.$eval('#cena', el => el.textContent)).trim();
-				let sqm = (await page.$eval('#cenakv', el => el.textContent)).trim();
-				const phone = (await page.$eval('.phone', el => el.textContent)).trim();
-				let area = (await page.$eval('.adParams div:first-child', el => el.textContent)).trim();
-				let floor = (await page.$eval('.adParams div:nth-child(2)', el => el.textContent)).trim();
-				let construction = (await page.$eval('.adParams div:nth-child(3)', el => el.textContent)).trim();
-				const description = (await page.$eval('#description_div', el => el.textContent)).trim();
-				const realtor = (await page.$eval('.AG .name', el => el.textContent)).trim();
-				const realtorLogo = (await page.$eval('.AG .logo img', el => el.src)).trim();
-				const realtorAddress = (await page.$eval('.AG .adress', el => el.textContent)).trim();
-
-				if (construction.includes(':')) {
-					construction = construction.split(':')[1].trim();
-					if (construction.length <= 3) {
-						console.log('construction.length was less than 3, skipping...');
+					if (isPriceUnavailable) {
+						console.log(`price unavailable for URL: ${url}, skipping...`);
 						continue;
 					}
-				}
 
-				if (area.includes(':')) {
-					area = area.split(':')[1].trim();
-				}
-				if (floor.includes(':')) {
-					floor = floor.split(':')[1].trim();
-				}
+					const images = [];
 
-				if (sqm.includes('(')) {
-					sqm = sqm.replace(/\(|\)/g, '');
+					const hasMultipleImages = await page.evaluate(() => {
+						const divIm = document.querySelector('div.im');
+						return divIm !== null;
+					});
+
+					if (hasMultipleImages) {
+						const imageSelectors = await page.$$eval('div.im a', els => els.map(el => el.dataset.link));
+						images.push(...imageSelectors.map(imgUrl => imgUrl.trim()));
+					} else {
+						const imgUrl = (await page.$eval('#bigPictureCarousel', el => el.src)).trim();
+						images.push(imgUrl);
+					}
+
+					await page.waitForSelector('.title');
+					await page.waitForSelector('.location');
+					await page.waitForSelector('#cena');
+					await page.waitForSelector('#cenakv');
+					await page.waitForSelector('#bigPictureCarousel');
+					await page.waitForSelector('.phone');
+					await page.waitForSelector('.adParams div:first-child');
+					await page.waitForSelector('.adParams div:nth-child(2)');
+					await page.waitForSelector('.adParams div:nth-child(3)');
+					await page.waitForSelector('#description_div');
+					await page.waitForSelector('.AG');
+					await page.waitForSelector('.AG .name');
+					await page.waitForSelector('.AG .logo img');
+					await page.waitForSelector('.AG .adress');
+
+					const title = (await page.$eval('.title', el => el.textContent)).trim();
+					const titleNoPrice = extractTitleWithoutPrice(title);
+
+					const location = (await page.$eval('.location', el => el.textContent)).trim();
+					const price = (await page.$eval('#cena', el => el.textContent)).trim();
+					let sqm = (await page.$eval('#cenakv', el => el.textContent)).trim();
+					const phone = (await page.$eval('.phone', el => el.textContent)).trim();
+					let area = (await page.$eval('.adParams div:first-child', el => el.textContent)).trim();
+					let floor = (await page.$eval('.adParams div:nth-child(2)', el => el.textContent)).trim();
+					let construction = (await page.$eval('.adParams div:nth-child(3)', el => el.textContent)).trim();
+					const description = (await page.$eval('#description_div', el => el.textContent)).trim();
+					const realtor = (await page.$eval('.AG .name', el => el.textContent)).trim();
+					const realtorLogo = (await page.$eval('.AG .logo img', el => el.src)).trim();
+					const realtorAddress = (await page.$eval('.AG .adress', el => el.textContent)).trim();
+
+					if (construction.includes(':')) {
+						construction = construction.split(':')[1].trim();
+						if (construction.length <= 3) {
+							console.log('construction.length was less than 3, skipping...');
+							continue;
+						}
+					}
+
+					if (area.includes(':')) {
+						area = area.split(':')[1].trim();
+					}
+					if (floor.includes(':')) {
+						floor = floor.split(':')[1].trim();
+					}
+
+					if (sqm.includes('(')) {
+						sqm = sqm.replace(/\(|\)/g, '');
+					}
+
+					const scrapedInfo = {
+						title: titleNoPrice,
+						location,
+						price,
+						sqm,
+						images,
+						phone,
+						area,
+						floor,
+						construction,
+						description,
+						url,
+						realtor,
+						realtorLogo,
+						realtorAddress,
+					};
+					console.log('DATA:', scrapedInfo);
+
+					realEstateData.push(scrapedInfo);
+				} catch (error) {
+					console.error(`Error scraping full data for listing: ${url}`, error);
 				}
-
-				const scrapedInfo = {
-					title: titleNoPrice,
-					location,
-					price,
-					sqm,
-					images,
-					phone,
-					area,
-					floor,
-					construction,
-					description,
-					url,
-					realtor,
-					realtorLogo,
-					realtorAddress,
-				};
-				console.log('Scraped Info:', scrapedInfo);
-
-				realEstateData.push(scrapedInfo);
-			} catch (error) {
-				console.error(`Error scraping full data for listing: ${url}`, error);
 			}
 		}
-
-		// await page.screenshot({ path: 'listings.png', fullPage: true });
 
 		await browser.close();
 		console.log('finished scraping!');
@@ -160,20 +169,20 @@ async function scrapeDataWithRetry() {
 			const realEstateData = await scrapeRealEstateData();
 
 			if (realEstateData) {
-				console.log(`Data scraped at Attempt ${++retryCount}`);
+				console.log(`data scraped at attempt ${++retryCount}`);
 				return realEstateData;
 			} else {
 				retryCount++;
 			}
 		} catch (error) {
-			console.error(`Error scraping real estate data (Attempt ${retryCount + 1}):`, error);
+			console.error(`error scraping real estate data (attempt ${retryCount + 1}):`, error);
 			retryCount++;
 
 			await delay(3000);
 		}
 	}
 
-	console.error('Max retries reached. Scraping failed.');
+	console.error('max retries reached, scraping failed...');
 	return null;
 }
 
