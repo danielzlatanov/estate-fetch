0df8b537e7765953b8488ad2148bcaa147a617c9
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-10-19 00:42:31 +0300

    changed loader to a unique blob-style design

--- frontend/estate-fetch/src/app/shared/spinner/spinner.component.ts
+++ frontend/estate-fetch/src/app/shared/spinner/spinner.component.ts
@@ -1,25 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-spinner',
   templateUrl: './spinner.component.html',
   styleUrls: ['./spinner.component.css'],
 })
-export class SpinnerComponent {
-  @Input() width = 16;
-  @Input() height = 16;
-
-  get spinnerClasses(): string {
-    return `
-      w-${this.width}
-      h-${this.height}
-      animate-spin
-      rounded-full
-      shadow-md
-      border-x-4
-      border-solid
-      border-t-transparent
-      border-blue-600
-    `;
-  }
-}
+export class SpinnerComponent {}
