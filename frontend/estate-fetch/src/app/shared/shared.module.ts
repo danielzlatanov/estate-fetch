f69ff4e13d015f3cd6cba470f0c2cf28eda27f75
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-10-24 10:47:01 +0300

    integrated lightbox functionality for image display

--- frontend/estate-fetch/src/app/shared/shared.module.ts
+++ frontend/estate-fetch/src/app/shared/shared.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SliderComponent } from './slider/slider.component';
 import { SpinnerComponent } from './spinner/spinner.component';
+import { LightboxModule } from 'ng-gallery/lightbox';
+import { GalleryModule } from 'ng-gallery';
 
 @NgModule({
   declarations: [SliderComponent, SpinnerComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, LightboxModule, GalleryModule],
   exports: [SliderComponent, SpinnerComponent],
 })
 export class SharedModule {}
--- frontend/estate-fetch/src/app/shared/slider/slider.component.ts
+++ frontend/estate-fetch/src/app/shared/slider/slider.component.ts
@@ -5,22 +5,35 @@ import {
   HostListener,
   Input,
   OnDestroy,
+  OnInit,
   ViewChild,
 } from '@angular/core';
 import KeenSlider, { KeenSliderInstance } from 'keen-slider';
+import { Gallery, GalleryItem, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.css'],
 })
-export class SliderComponent implements AfterViewInit, OnDestroy {
+export class SliderComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('sliderRef') sliderRef!: ElementRef<HTMLElement>;
   @Input() images: string[] = [];
+  items: GalleryItem[] = [];
 
   currentSlide = 0;
   dotHelper: Array<number> = [];
   slider: KeenSliderInstance | null = null;
+
+  constructor(private gallery: Gallery) {}
+
+  ngOnInit() {
+    this.items = this.images.map(
+      (image) => new ImageItem({ src: image, thumb: image })
+    );
+    const galleryRef = this.gallery.ref();
+    galleryRef.load(this.items);
+  }
 
   @HostListener('document:keydown', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) {
