de92a1e83a8ce965213fa7aaf5a2df0067dd4f02
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-10-24 11:42:44 +0300

    implemented emoji removal for property descriptions

--- backend/services/scraper.js
+++ backend/services/scraper.js
@@ -49,7 +49,7 @@ async function scrapeRealEstateData() {
 		const end = performance.now();
 		const totalTimeInMinutes = Math.floor((end - start) / 60000);
 		const remainingSeconds = ((end - start) % 60000) / 1000;
-		
+
 		console.log(
 			`finished scraping:\nscraping took ${totalTimeInMinutes} minutes and ${remainingSeconds.toFixed(2)} seconds`
 		);
@@ -146,12 +146,13 @@ async function scrapeDataFromUrls(validListingUrls, page) {
 			let area = (await page.$eval('.adParams div:first-child', el => el.textContent)).trim();
 			let floor = (await page.$eval('.adParams div:nth-child(2)', el => el.textContent)).trim();
 			let construction = (await page.$eval('.adParams div:nth-child(3)', el => el.textContent)).trim();
-			let description = (await page.$eval('#description_div', el => el.textContent)).trim();
+			let description = await page.$eval('#description_div', el => el.textContent);
 			const realtor = (await page.$eval('.AG .name', el => el.textContent)).trim();
 			const realtorLogo = (await page.$eval('.AG .logo img', el => el.src)).trim();
 			const realtorAddress = (await page.$eval('.AG .adress', el => el.textContent)).trim();
 
-			description = description.replace('Виж по-малко... Виж повече', '').trim();
+			description = description.replace('Виж по-малко... Виж повече', '');
+			description = removeEmojis(description).trim();
 
 			if (construction.includes(':')) {
 				construction = construction.split(':')[1].trim();
@@ -225,6 +226,12 @@ async function scrapeDataWithRetry() {
 
 	console.error('max retries reached, scraping failed...');
 	return null;
+}
+
+function removeEmojis(string) {
+	const regex =
+		/(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
+	return string.replace(regex, '');
 }
 
 function delay(ms) {
