63d15e5dbf895f743f3cd9f6f21204afe8683ff0
danielzlatanov <110429874+danielzlatanov@users.noreply.github.com>
2023-11-10 17:48:14 +0200

    shared nav links into a helper

---
+++ frontend/estate-fetch/src/app/shared/helpers/navLinks.ts
@@ -0,0 +1,7 @@
+import { INavLink } from '../interfaces/navLink';
+
+export const NAV_LINKS: INavLink[] = [
+  { label: 'Home', route: '/', fragment: 'home' },
+  { label: 'Features', route: '/', fragment: 'features' },
+  { label: 'FAQ', route: '/', fragment: 'faq' },
+];
